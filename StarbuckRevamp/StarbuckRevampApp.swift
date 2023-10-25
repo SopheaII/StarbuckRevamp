464d09ad5225e9ae7f78a2274e1e417569f451e0
SaoSophea <sophea.sao@wbfinance.com.kh>
2023-10-25 15:01:56 +0700

    Initial Commit

---
+++ StarbuckRevamp/StarbuckRevampApp.swift
@@ -0,0 +1,17 @@
+//
+//  StarbuckRevampApp.swift
+//  StarbuckRevamp
+//
+//  Created by Sao Sophea on 25/10/23.
+//
+
+import SwiftUI
+
+@main
+struct StarbuckRevampApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
